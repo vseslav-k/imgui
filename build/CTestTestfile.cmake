9352d5ec5258c8ffbaa319776a9ec30f5f9d5fdc
vseslav-k <vseslavvak@mail.ru>
2026-01-19 20:34:31 -0800

    removed build folder

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Libraries/imgui
-# Build directory: C:/Libraries/imgui/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
